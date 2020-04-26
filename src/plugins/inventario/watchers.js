6723284fc181fe9cec280b0f794ad662c2df49fd
rubengp99 <rdgp99@gmail.com>
2020-04-26 15:36:39 -0400

    Cambios de indentacion en plugins > watchers

--- src/plugins/inventario/watchers.js
+++ src/plugins/inventario/watchers.js
@@ -2,30 +2,30 @@ import _ from 'lodash';
 
 const search = async function(after){
     if(after == "" && this.$route.name === 'concepto')
-      this.$router.push('/Inventario');
+        this.$router.push('/Inventario');
     if (this.grupo === "" && this.grupo === "" && this.$route.name !== 'concepto'){
-      if(this.search === "" && this.filteredConcepts.length > 0){
-        this.table.products = [];
-        this.loading = true;
-        this.getConcept(false, after,  this.apiConcepts.data.data);
-        this.table.totalConceptos = this.apiConcepts.data.totalCount;
-      }else
-        return;
+        if(this.search === "" && this.filteredConcepts.length > 0){
+            this.table.products = [];
+            this.loading = true;
+            this.getConcept(false, after,  this.apiConcepts.data.data);
+            this.table.totalConceptos = this.apiConcepts.data.totalCount;
+        }else
+            return;
     }else if (this.grupo !== "" || this.grupo !== "" && this.$route.name == 'concepto'){
-      if(this.search === ""){
-        this.loading = true;
-        this.table.products = [];
-        if(this.subgrupo !== ""){
-          this.filteredConcepts = ((this.search === "" ) ? this.apiConcepts.data.data : this.filteredConcepts).filter(c => c.subgrupos_id === this.subgrupo.id || c.adm_subgrupos_id === this.subgrupo.id);
-        }else if(this.grupo !== ""){
-          this.filteredConcepts = ((this.search === "") ? this.apiConcepts.data.data : this.filteredConcepts).filter(c => c.grupos_id === this.grupo.id || c.adm_grupos_id === this.grupo.id);
-        }
-        this.table.totalConceptos = this.filteredConcepts.length; 
-        await this.getConcept(false, after,  this.filteredConcepts);
-      }else
-        return;
+        if(this.search === ""){
+            this.loading = true;
+            this.table.products = [];
+            if(this.subgrupo !== ""){
+                this.filteredConcepts = ((this.search === "" ) ? this.apiConcepts.data.data : this.filteredConcepts).filter(c => c.subgrupos_id === this.subgrupo.id || c.adm_subgrupos_id === this.subgrupo.id);
+            }else if(this.grupo !== ""){
+                this.filteredConcepts = ((this.search === "") ? this.apiConcepts.data.data : this.filteredConcepts).filter(c => c.grupos_id === this.grupo.id || c.adm_grupos_id === this.grupo.id);
+            }
+            this.table.totalConceptos = this.filteredConcepts.length; 
+            await this.getConcept(false, after,  this.filteredConcepts);
+        }else
+            return;
     }else
-      return;
+        return;
   };
 
 const goSearch = _.debounce(async function () {
@@ -42,7 +42,7 @@ const singleExpand  = function(v) {
     if (!v) return
     // Single expand enabled. Hide all but the first expanded item
     this.expanded.forEach((item, i) => {
-      if (i > 0) this.closeExpand(item)
+        if (i > 0) this.closeExpand(item)
     })
   };
 
@@ -71,7 +71,7 @@ const grupo = _.debounce(function() {
     this.table.totalConceptos = Math.ceil(this.filteredConcepts.length);
     this.table.pageCount = Math.ceil(this.table.totalConceptos / this.table.itemsPerPage);
     this.getConcept(false,this.search,this.filteredConcepts, (this.search === ""));
-  },555);
+},555);
 
 const subgrupo = _.debounce(function() {
     //los watchers a veces pueden caer en bucles, es por ello que cuando se limpia la variable es necesaria una
@@ -95,7 +95,7 @@ const clear = _.debounce(async function(){
     //los watchers a veces pueden caer en bucles, es por ello que cuando se limpia la variable es necesaria una
     //condición que nos saque del procedimiento.
     if(this.$route.name === 'concepto')
-      this.$router.push('/Inventario');
+        this.$router.push('/Inventario');
     if (this.search === "" && this.grupo === "" && this.subgrupo === "" || this.$route.name === 'concepto') return;
     this.table.products = [];
     this.loading = true;
